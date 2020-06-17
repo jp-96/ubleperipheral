ad852219993dd740ff67e79375c8de13c0d409ab
jp-96 <h.kuroyanagi.css@gmail.com>
2020-06-17 21:25:17 +0900

    1.0.0

--- bleperipheral/__init__.py
+++ bleperipheral/__init__.py
@@ -4,3 +4,7 @@ bleperipheral package
 '''
 from bleperipheral.ble_peripheral import BLEPeripheral
 from bleperipheral.ble_advertising import advertising_payload, decode_field, decode_name, decode_services, decode_service_data
+
+__version__ = '1.0.0'
+
+__all__ = ['BLEPeripheral', 'advertising_payload', 'decode_field', 'decode_name', 'decode_services', 'decode_service_data']
